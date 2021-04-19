17f6452a953c4345c55b8973cd4ac815e0b3e9ff
Romain Clemencon <romain.clemencon@mycit.ie>
2021-04-19 13:38:41 +0100

    Final version of my approach. Using both example given, from assignment and lecture. Also added a random example with random length and values. The output on screen could be updated with printing how many time a rod is used, instead of printing it each time eventually.

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,37 @@ public class Main {
     public static void main(String[] args) {
 
         /* Given example of the value of the roll-piece
-            Using an array of double since it has a better time complexity than Lists
-            rollPrices: array of doubles since values are decimal
+            Using an array since it has a better time complexity than Lists
+            rollPrices: array of doubles with the values given in the example
+            rodCut: array of int using example given in the lecture
          */
         double[] rollPieces = {1.2, 3, 5.8, 0, 10.1};
         double[] rodCut = {1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-        int rollLength = rollPieces.length;
 
+
+        // Creating instance of Bottom Up algorithm
         PaperRollCuttingBottomUp bestSolution = new PaperRollCuttingBottomUp();
 
-        bestSolution.bottomUpCut(rollPieces, 5);
+        // Assignment example
+        System.out.println("Example 1: Paper Roll Cutting\n------------------------------");
+        bestSolution.bottomUpCut(rollPieces, rollPieces.length);
+
+        // Lecture example
+        System.out.println("Example 2: Rod Cutting Problem\n------------------------------");
+        bestSolution.bottomUpCut(rodCut, rodCut.length);
+
+        // Random example with a length from 10 to 30
+        int randLength = (int) ((Math.random() * (30 - 10)) + 10);
+
+        // Initializing random values array
+        double[] randArray = new double[randLength];
+
+        // Assigning random values
+        for (double i = 1; i <= randLength; i++) randArray[(int)i-1] = Math.random() * ((2*i)-i)+i;
+
+        // Printing on screen
+        System.out.println("Example 3: Random dynamic values\n--------------------------------");
+        bestSolution.bottomUpCut(randArray, randArray.length);
 
     }
 }
--- src/PaperRollCuttingBottomUp.java
+++ src/PaperRollCuttingBottomUp.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 public class PaperRollCuttingBottomUp {
 
     void bottomUpCut(double[] rollPieces, int rollLength){
 
         // Array to memorize dynamically the sub-problems
         double[] subRolls = new double[rollLength+1];
+        int[] firstCut = new int[rollLength+1];
 
         // Initial sub value of length 0 is 0
         subRolls[0] = 0;
@@ -14,14 +17,70 @@ public class PaperRollCuttingBottomUp {
             double maxRollValue = Integer.MIN_VALUE;
 
             // For each sub value - checking if it's higher
-            for (int j = 0; j < i; j++) maxRollValue = Math.max(maxRollValue, rollPieces[j] + subRolls[i-j-1]);
+            for (int j = 0; j < i; j++){
+                // If it's higher than the previous highest value, replace it with the new value
+                // Indicate what's the 1st best cut to use
+                if (maxRollValue < rollPieces[j] + subRolls[i-j-1]){
+                    maxRollValue = rollPieces[j] + subRolls[i-j-1]; firstCut[i] = j+1;
+                }
+            }
 
             // Assigning the maxvalue previously found
             subRolls[i] = maxRollValue;
         }
 
-        // Returning the requested best value for the roll length
-        System.out.println("The best price for length " + rollLength + " is: " + subRolls[rollLength]);
+        //Printing the lengths
+        System.out.printf("|%-7s|", "Length");
+        for (int i = 1; i <= rollLength; i++) {
+            System.out.printf("%6d", i);
+        }
+
+        //Printing the prices given
+        System.out.printf("\n|%-7s|", "Price");
+        for (int i = 0; i < rollLength; i++) {
+            System.out.printf("%6.1f", rollPieces[i]);
+        }
+
+        //Printing the best prices possible
+        System.out.printf("\n|%-7s|", "Best €");
+        for (int i = 0; i < rollLength; i++) {
+            System.out.printf("%6.1f", subRolls[i+1]);
+        }
+
+        //Printing the best prices possible
+        System.out.printf("\n|%-7s|", "1st Cut");
+        for (int i = 0; i < rollLength; i++) {
+            System.out.printf("%6d", firstCut[i+1]);
+        }
+
+        // Printing roll cut and their type
+        System.out.println("\n\nCut for each rolls/rods:");
+
+        // For each possible length, printing each cuts starting with the first one
+        for (int i = 0; i < rollLength; i++) {
+            System.out.print("- length " + (i+1) + " = ");
+
+            // Starting from the length 1 to n
+            int checkRoll = i+1;
+            while (checkRoll > 0){
+                // We first check the first cut to use, which was the last found
+                System.out.print(firstCut[checkRoll] + " ");
+                // Printing dash to look nice , but could be better or different
+                for (int j = 0; j < firstCut[checkRoll]; j++) {
+                    System.out.print("-");
+                }
+                // Now we reduce the length of the roll using the last roll we cut
+                checkRoll = checkRoll - firstCut[checkRoll];
+
+                // If the roll still can't be cut, it will loop and let's print a + to make it nicer
+                if (checkRoll> 0) System.out.print(" + ");
+            }
+            // Spacing
+            System.out.println();
+        }
+
+
+
     }
 
 
